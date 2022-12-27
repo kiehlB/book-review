e056f518c62d690b8ccb2b0735499418dc7ba6b6
kiehlb <thekiehlb@gmail.com>
2022-12-28 05:42:33 +0900

    update'

--- server/src/env.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({
-  path: path?.resolve(
-    process.cwd(),
-    process.env.NODE_ENV === 'development' ? '.env.development' : '.env',
-  ),
-});
