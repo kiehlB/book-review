e9f1ac2762364425aa0416979379807eb6f545f4
kiehlB <thekiehlb@gmail.com>
2023-01-22 21:46:44 +0900

    update

--- next.config.js
+++ next.config.js
@@ -4,11 +4,24 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: '/api',
-        destination: 'https://api.bookreview.pro/api',
+        // matching all API routes
+        source: '/api/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
     ];
   },
