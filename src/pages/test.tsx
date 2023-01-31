0693c08f014962adf98673f8e90a5f3deb1c22f3
kiehlB <thekiehlb@gmail.com>
2023-02-01 04:01:54 +0900

    update

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -153,10 +153,7 @@ export default function Test() {
           </symbol>
         </svg>
         {/* dribbble */}
-        <a
-          className="dribbble"
-          href="https://dribbble.com/shots/8082836-Paw-Clap-Button"
-          target="_blank">
+        <a className="dribbble" href="https://dribbble.com/shots/8082836-Paw-Clap-Button">
           <img
             src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg"
             alt=""
