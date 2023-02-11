f4796807fefe4b3c20a6d747979eef47e9b396c9
thekieB <thekiehlb@gmail.com>
2023-02-11 10:30:30 +0900

    update

---
+++ src/components/write/hooks/useGetSinglePost.tsx
@@ -0,0 +1,22 @@
+import { useQuery, gql, useMutation } from '@apollo/client';
+import { GET_Post } from '../../../lib/graphql/posts';
+import { useRouter } from 'next/router';
+
+export default function useGetPost() {
+  const router = useRouter();
+
+  const {
+    loading: singlePostLoding,
+    error: singlePostError,
+    data: singlePostData,
+  } = useQuery(GET_Post, {
+    variables: { id: router?.query?.id?.toString() },
+    skip: !router?.query?.id,
+  });
+
+  return {
+    singlePostLoding,
+    singlePostError,
+    singlePostData,
+  };
+}
