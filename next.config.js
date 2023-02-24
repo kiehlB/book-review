4a7938a54d2b071881f209f028321a9bf5613da4
kiehlB <thekiehlb@gmail.com>
2023-02-25 04:22:29 +0900

    update

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:4000/:path*',
-      },
-    ];
-  },
+
   env: {
     API_URL: process.env.API_URL,
     KAKAO: process.env.KAKAO,
