1196ff189f29b2faed75259be19c591966375282
kiehlB <thekiehlb@gmail.com>
2023-02-26 03:25:16 +0900

    update

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -197,7 +197,9 @@ const AuthForm: React.FC<AuthFormProps> = ({
       </div>
 
       <div className="flex justify-between w-[50%] mx-auto pt-3">
-        <Link href="http://localhost:4000/api/v2/auth/redirect/google" passHref={true}>
+        <Link
+          href="https://api.bookreview.pro/api/v2/auth/redirect/google"
+          passHref={true}>
           <Google />
         </Link>
       </div>
