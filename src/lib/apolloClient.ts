16c30b6e3a68efd93045a40c6e6ce816229507ce
thekieB <thekiehlb@gmail.com>
2023-04-01 21:08:44 +0900

    update:

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -31,9 +31,12 @@ const linkOnError = onError(
     }
   },
 );
-
+const prod =
+  process.env.NODE_ENV == 'development'
+    ? 'http://localhost:4000/graphql'
+    : 'https://api.bookreview.pro/graphql';
 const httpLink = new HttpLink({
-  uri: process.env.API_URL, // Server URL (must be absolute)
+  uri: prod, // Server URL (must be absolute)
   credentials: 'include', // Additional fetch() options like `credentials` or `headers`,
 });
 
